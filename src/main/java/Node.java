badc8227589db458ee89805a6c2f9f86c2a05665
NicolasPabloW <nicokido2011@gmail.com>
2021-06-15 20:41:55 -0300

    Agrego metodos

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
- public Node next;
+ public List<Node> sons = new ArrayList<>();
+ public Node before;
  public String content ;
+
+ public Node(Node before){
+  this.before = before;
+ }
+
+ public void createSonNode(){
+  this.sons.add(new Node(this));
+ }
 }
