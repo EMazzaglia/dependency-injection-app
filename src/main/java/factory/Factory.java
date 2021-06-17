45a8beb97adb44b767b4c2be33d65053c7050506
Daniel <dnovacovich@santandertecnologia.com.ar>
2021-06-16 21:34:10 -0300

    mas comentarios

--- src/main/java/factory/Factory.java
+++ src/main/java/factory/Factory.java
@@ -6,31 +6,33 @@ import java.lang.reflect.Proxy;
 
 public class Factory {
 
+    // Comentarios realizados para el caso: Factory.getObject(FordFiesta.class);
     public static Object getObject(Class<?> klass) throws InstantiationException, IllegalAccessException {
-        // Generamos una nueva instancia requerida (Ej. FordFiesta.class)
+        // Generamos una nueva instancia de FordFiesta.class
         Object newObject = klass.newInstance();
 
-        // Iteramos sobre todos los campos de la clase. (Ej. FordFiesta.class)
+        // Iteramos sobre todos los campos de la clase FordFiesta
         for (Field field : klass.getDeclaredFields()) {
-            // (Primera iteración Field = motor)
-            // Obtenemos la anotación Injected del campo. (Ej. Motor)
+            // Field = motor
+            // Obtenemos la anotación Injected del campo motor
             Injected injected = field.getAnnotation(Injected.class);
 
-            // Obtenemos la clase a inyectar de la anotación. (Ej. ToyotaMotor.class)
-            Class<?> injectedClass = injected.implementation();
+            // Obtenemos la clase a inyectar desde la anotación. (ToyotaMotor.class)
+            Class<?> classToInject = injected.implementation();
 
-            // Generamos una instancia de la clase del campo.  (Ej. ToyotaMotor.class)
-            Object injectObj = injectedClass.newInstance();
+            // Generamos una instancia de ToyotaMotor
+            Object objToInject = classToInject.newInstance();
 
-            // Ponemos el campo en publico para poder modificarlo
+            // Ponemos el campo en publico para poder modificarlo. field = motor
             field.setAccessible(true);
-            
-            // Inyectamos la nueva instancia del campo en la clase padre.
+
+            // Ahora seteamos el campo motor de nuestra instancia de FordFiesta con la instancia de Motor
             // Es decir: FordFiesta -> motor = new ToyotaMotor()
-            field.set(newObject, injectObj);
+            field.set(newObject, objToInject);
             // Ponemos el campo en privado para no dejarlo publico.
             field.setAccessible(false);
         }
+        // Retornamos el objeto FordFiesta con un motor Toyota
         return newObject;
     }
 }
